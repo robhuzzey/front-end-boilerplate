a744e3a4ff4286314537674afbc06282b4e1d81b
Rob Huzzey <robert.huzzey@holidayextras.com>
2017-02-25 00:28:57 +0000

    Init

---
+++ src/Application.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import HelloWorld from './components/HelloWorld.jsx'
+
+(() => {
+  ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('application')
+  )
+})()
+
